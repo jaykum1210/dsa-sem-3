aa88b6343ee629d3461c55a68bb9238438d9d4e5
jaykum1210 <jaykumawat1210@gmail.com>
2025-11-06 14:50:58 +0530

    add files

---
+++ Lab Practice/gcd.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+    {
+        return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+int main()
+{
+    int a, b;
+    printf("Enter number = ");
+    scanf("%d %d", &a, &b);
+    int c = gcd(a, b);
+    printf("GCD of %d and %d = %d\n", a, b, c);
+    return 0;
+}
