fd625473852f8358a528b1953e878376dba82ef7
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-10 10:41:04 +0530

    add files

---
+++ ESE practical 1/bubble.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    printf("Enter size = ");
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for (int i = 0; i < a-1; i++)
+    {
+        for (int j = 0; j < a-i-1; j++)
+        {
+            if (b[j]>b[j+1])
+            {
+                int temp = b[j];
+                b[j] = b[j+1];
+                b[j+1] = temp;
+            }
+        }
+    }
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",b[i]);
+    }
+    return 0;
+}
