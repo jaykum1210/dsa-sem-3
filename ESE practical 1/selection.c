9f3206a0a8250e167396c975075e2f325429b43b
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-10 09:51:21 +0530

    add files

---
+++ ESE practical 1/selection.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for (int i = 0; i < a; i++)
+    {
+        int min = i;
+        for (int j = i; j <a; j++)
+        {
+            if (b[min]<b[j])
+            {
+                min = j;
+            }
+            int temp = b[min];
+            b[min] = b[j];
+            b[j] = temp;
+        }
+    }
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",b[i]);
+    }
+    return 0;
+}
