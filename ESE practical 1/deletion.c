3ff79b7f1491c0e4a951e177ef90f08c368b903b
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-10 08:11:46 +0530

    add files

---
+++ ESE practical 1/deletion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    printf("Enter %d elements = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    int pos;
+    printf("Enter position = ");
+    scanf("%d",&pos);
+    for (int i = pos-1; i < a; i++)
+    {
+        b[i] = b[i+1];
+    }
+    a--;
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",b[i]);
+    }
+    return 0;
+}
