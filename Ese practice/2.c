58009bb735ac6393613e97c005ba762c51d82487
jaykum1210 <jaykumawat1210@gmail.com>
2025-12-11 08:12:44 +0530

    add files

---
+++ Ese practice/2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    printf("Enter size = ");
+    scanf("%d",&a);
+    int b[a];
+    printf("Enter %d elments = ",a);
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d",&b[i]);
+    }
+    for (int i = 0; i < a; i++)
+    {
+        int key = b[i];
+        int j = i-1;
+        while (j>=0 && b[j]>key)
+        {
+            b[j+1] = b[j];
+            j--;
+        }
+        b[j+1] = key;
+    }
+    printf("Array = ");
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ",b[i]);
+    }
+    return 0;
+}
